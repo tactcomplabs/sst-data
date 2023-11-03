779acf30ab4aa6d509ea57161fd46cae168c4af9
jleidel <john.leidel@gmail.com>
2023-11-03 17:34:50 -0500

    adding first test code; compiles but does not load

---
+++ src/test/statOutputTest.cc
@@ -0,0 +1,58 @@
+#include <sst/core/sst_config.h>
+#include <sst/core/stringize.h>
+#include "statOutputTest.h"
+//#include <sst/core/simulation_impl.h>
+
+namespace SST::Statistics{
+
+StatisticOutputTest::StatisticOutputTest(Params& outputParameters)
+  : SSTDataBase(outputParameters){
+//    m_useCompression = outputParameters.find<bool>("compressed");
+    // Announce this output object's name
+    //Output& out      = Simulation_impl::getSimulationOutput();
+    //out.verbose(CALL_INFO, 1, 0, " : StatisticOutputTest enabled...\n");
+    setStatisticOutputName("StatisticOutputTest");
+}
+
+bool StatisticOutputTest::checkOutputParameters(){
+  return true;
+}
+
+void StatisticOutputTest::printUsage(){
+  Output out("", 0, 0, Output::STDOUT);
+  out.output(" : Usage - Sends all statistic output compilation test.\n");
+}
+
+void StatisticOutputTest::startOfSimulation(){
+}
+
+void StatisticOutputTest::endOfSimulation(){
+}
+
+void StatisticOutputTest::implStartOutputEntries(StatisticBase* statistic){
+}
+
+void StatisticOutputTest::implStopOutputEntries(){
+}
+
+void StatisticOutputTest::outputField(fieldHandle_t fieldHandle, int32_t data){
+}
+
+void StatisticOutputTest::outputField(fieldHandle_t fieldHandle, uint32_t data){
+}
+
+void StatisticOutputTest::outputField(fieldHandle_t fieldHandle, int64_t data){
+}
+
+void StatisticOutputTest::outputField(fieldHandle_t fieldHandle, uint64_t data){
+}
+
+void StatisticOutputTest::outputField(fieldHandle_t fieldHandle, float data){
+}
+
+void StatisticOutputTest::outputField(fieldHandle_t fieldHandle, double data){
+}
+
+};  // end SST::Statistics
+
+// EOF
---
+++ src/test/statOutputTest.h
@@ -0,0 +1,77 @@
+#ifndef STAT_OUTPUT_TEST_H_
+#define STAT_OUTPUT_TEST_H_
+
+#include <sst/core/sst_types.h>
+#include <sst/core/statapi/statoutput.h>
+
+namespace SST::Statistics{
+
+class SSTDataBase : public StatisticFieldsOutput{
+public:
+  SST_ELI_DECLARE_BASE(SST::Statistics::SSTDataBase);
+  //SST_ELI_REGISTER_EXTERN(StatisticFieldsOutput, SSTDataBase)
+  SSTDataBase(Params& outputParameters)
+    : StatisticFieldsOutput(outputParameters){}
+protected:
+  bool checkOutputParameters()  = 0;
+  void printUsage() = 0;
+  void startOfSimulation() = 0;
+  void endOfSimulation() = 0;
+  void implStartOutputEntries(StatisticBase* statistic) = 0;
+  void implStopOutputEntries() = 0;
+  void outputField(fieldHandle_t fieldHandle, int32_t data) = 0;
+  void outputField(fieldHandle_t fieldHandle, uint32_t data) = 0;
+  void outputField(fieldHandle_t fieldHandle, int64_t data) = 0;
+  void outputField(fieldHandle_t fieldHandle, uint64_t data) = 0;
+  void outputField(fieldHandle_t fieldHandle, float data) = 0;
+  void outputField(fieldHandle_t fieldHandle, double data) = 0;
+
+  virtual bool acceptsGroups() const override = 0;
+
+  SSTDataBase() { ; }
+
+};
+
+class StatisticOutputTest : public SSTDataBase{
+public:
+  SST_ELI_REGISTER_DERIVED(
+    SSTDataBase,
+    StatisticOutputTest,
+    "sstData",
+    "statoutputtest",
+    SST_ELI_ELEMENT_VERSION(1,0,0),
+    "Output test"
+  )
+
+  StatisticOutputTest(Params& outputParameters);
+
+protected:
+  bool checkOutputParameters() override;
+
+  void printUsage() override;
+
+  void startOfSimulation() override;
+
+  void endOfSimulation() override;
+
+  void implStartOutputEntries(StatisticBase* statistic) override;
+
+  void implStopOutputEntries() override;
+
+  void outputField(fieldHandle_t fieldHandle, int32_t data) override;
+  void outputField(fieldHandle_t fieldHandle, uint32_t data) override;
+  void outputField(fieldHandle_t fieldHandle, int64_t data) override;
+  void outputField(fieldHandle_t fieldHandle, uint64_t data) override;
+  void outputField(fieldHandle_t fieldHandle, float data) override;
+  void outputField(fieldHandle_t fieldHandle, double data) override;
+
+  bool acceptsGroups() const override { return true; }
+
+  StatisticOutputTest()  : SSTDataBase() { ; }
+
+private:
+};  // end StatOutputTest
+};  // end SST::Statistics
+
+#endif
+// EOF
