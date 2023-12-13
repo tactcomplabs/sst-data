f8d53531cb6e4a82ef076a8da1834eeb7e6f58c5
ct-clmsn <ct.clmsn@gmail.com>
2023-12-13 11:52:39 -0500

    added arrow

--- src/test/statOutputTest.cc
+++ src/test/statOutputTest.cc
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <sst/core/sst_config.h>
 #include <sst/core/stringize.h>
 #include "statOutputTest.h"
@@ -6,11 +7,11 @@
 namespace SST::Statistics{
 
 StatisticOutputTest::StatisticOutputTest(Params& outputParameters)
-  : SSTDataBase(outputParameters){
-//    m_useCompression = outputParameters.find<bool>("compressed");
-    // Announce this output object's name
+  : SSTDataBase(outputParameters), batches(), builders(), schemas() {
+  //m_useCompression = outputParameters.find<bool>("compressed");
+  //Announce this output object's name
   //Output& out      = Simulation_impl::getSimulationOutput();
-  std::cout << "BOGUS" << std::endl;
+  //std::cout << "BOGUS" << std::endl;
   //Output out("", 0, 0, Output::STDOUT);
   //out.verbose(CALL_INFO, 1, 0, " : StatisticOutputTest enabled...\n");
   setStatisticOutputName("StatisticOutputTest");
@@ -33,36 +34,140 @@ void StatisticOutputTest::endOfSimulation(){
   std::cout << "endOfSimulation" << std::endl;
 }
 
+static bool fieldTypeToArrowField(std::vector<StatisticOutputTest::ArrowBuilders> & builders, std::vector<std::shared_ptr<arrow::Field>> & fields, std::string const& field_str, const char * ft_cstr) {
+    if(std::strncmp(ft_cstr, "int32_t", 7)) {
+        fields.push_back(arrow::field(field_str, arrow::int32()));
+        builders.push_back(arrow::Int32Builder{});
+        return true;
+    }
+    else if(std::strncmp(ft_cstr, "int64_t", 7)) {
+        fields.push_back(arrow::field(field_str, arrow::int64()));
+        builders.push_back(arrow::Int64Builder{});
+        return true;
+    }
+    else if(std::strncmp(ft_cstr, "uint32_t", 8)) {
+        fields.push_back(arrow::field(field_str, arrow::uint32()));
+        builders.push_back(arrow::UInt32Builder{});
+        return true;
+    }
+    else if(std::strncmp(ft_cstr, "uint64_t", 8)) {
+        fields.push_back(arrow::field(field_str, arrow::uint64()));
+        builders.push_back(arrow::UInt64Builder{});
+        return true;
+    }
+    else if(std::strncmp(ft_cstr, "float", 5)) {
+        fields.push_back(arrow::field(field_str, arrow::float32()));
+        builders.push_back(arrow::FloatBuilder{});
+        return true;
+    }
+    else if(std::strncmp(ft_cstr, "double", 6)) {
+        fields.push_back(arrow::field(field_str, arrow::float64()));
+        builders.push_back(arrow::DoubleBuilder{});
+        return true;
+    }
+
+    fields.push_back(arrow::field(field_str, arrow::null()));
+    builders.push_back(arrow::NullBuilder{});
+    return false;
+};
+
+// https://arrow.apache.org/docs/python/api/datatypes.html
+//
+// https://arrow.apache.org/cookbook/cpp/create.html#create-arrays-from-standard-c
+//
 void StatisticOutputTest::implStartOutputEntries(StatisticBase* statistic){
-  std::cout << "implStartOutputEntries" << std::endl;
+
+  const std::string m_currentComponentName  = statistic->getCompName();
+  const std::string m_currentStatisticName  = statistic->getStatName();
+  const std::string m_currentStatisticSubId = statistic->getStatSubId();
+  const std::string m_currentStatisticType  = statistic->getStatTypeName();
+
+  std::cout << "implStartOutputEntries" << std::endl
+            << 1 << m_currentComponentName << '\t'
+            << 2 << m_currentStatisticName << '\t'
+            << 3 << m_currentStatisticSubId << '\t'
+            << 4 << m_currentStatisticType << std::endl;
+
+  auto arr = getFieldInfoArray();
+  SST::Statistics::StatisticFieldInfo * val = nullptr;
+  fieldType_t ft;
+  bool rv = false;
+
+  std::vector<std::shared_ptr<arrow::Field>> fields;
+  fields.reserve(arr.size());
+  builders.reserve(arr.size());
+
+  for(FieldInfoArray_t::iterator itr = arr.begin(); itr != arr.end(); itr++) {
+    val = *itr;
+    ft = val->getFieldType();
+    StatisticFieldTypeBase const* ptr = StatisticFieldTypeBase::getField(ft);
+    rv = fieldTypeToArrowField(builders, fields, val->getFieldName(), ptr->fieldName());
+    //std::cout << "***\t" << val->getStatName() << ' ' << val->getFieldName() << ' ' << val->getFieldUniqueName() << ' ' << ptr->fieldName() << ' ' << ptr->shortName() << std::endl;
+  }
+  
+  schemas.emplace_back(arrow::schema(fields));
 }
 
-void StatisticOutputTest::implStopOutputEntries(){
+void StatisticOutputTest::implStopOutputEntries() {
   std::cout << "implStopOutputEntries" << std::endl;
+
+  std::vector<std::shared_ptr<arrow::Array>> arrays;
+  arrays.reserve(builders.size());
+
+  int64_t max_length = 0;
+  for(auto i = 0; i < builders.size(); ++i) {
+     max_length = std::max(max_length, arrays[i]->length());
+
+     if(std::holds_alternative<arrow::Int32Builder>(builders[i])) {
+       arrays.push_back(std::get<arrow::Int32Builder>(builders[i]).Finish().ValueOrDie());
+     }
+     else if(std::holds_alternative<arrow::Int64Builder>(builders[i])) {
+       arrays.push_back(std::get<arrow::Int64Builder>(builders[i]).Finish().ValueOrDie());
+     }
+     else if(std::holds_alternative<arrow::UInt32Builder>(builders[i])) {
+       arrays.push_back(std::get<arrow::UInt32Builder>(builders[i]).Finish().ValueOrDie());
+     }
+     else if(std::holds_alternative<arrow::UInt64Builder>(builders[i])) {
+       arrays.push_back(std::get<arrow::UInt64Builder>(builders[i]).Finish().ValueOrDie());
+     }
+     else if(std::holds_alternative<arrow::FloatBuilder>(builders[i])) {
+       arrays.push_back(std::get<arrow::FloatBuilder>(builders[i]).Finish().ValueOrDie());
+     }
+     else if(std::holds_alternative<arrow::DoubleBuilder>(builders[i])) {
+       arrays.push_back(std::get<arrow::DoubleBuilder>(builders[i]).Finish().ValueOrDie());
+     }
+     else if(std::holds_alternative<arrow::NullBuilder>(builders[i])) {
+       arrays.push_back(std::get<arrow::NullBuilder>(builders[i]).Finish().ValueOrDie());
+     }
+  };
+
+  auto res = arrow::RecordBatch::Make(schemas.back(), max_length, arrays);
+  batches.emplace_back(res);
+  builders.clear();
 }
 
 void StatisticOutputTest::outputField(fieldHandle_t fieldHandle, int32_t data){
-  std::cout << "outputField-int" << std::endl;
+  std::get<arrow::Int32Builder>(builders[fieldHandle]).Append(data);
 }
 
 void StatisticOutputTest::outputField(fieldHandle_t fieldHandle, uint32_t data){
-  std::cout << "outputField-uint" << std::endl;
+  std::get<arrow::UInt32Builder>(builders[fieldHandle]).Append(data);
 }
 
 void StatisticOutputTest::outputField(fieldHandle_t fieldHandle, int64_t data){
-  std::cout << "outputField-long" << std::endl;
+  std::get<arrow::Int64Builder>(builders[fieldHandle]).Append(data);
 }
 
 void StatisticOutputTest::outputField(fieldHandle_t fieldHandle, uint64_t data){
-  std::cout << "outputField-ulong" << std::endl;
+  std::get<arrow::UInt64Builder>(builders[fieldHandle]).Append(data);
 }
 
 void StatisticOutputTest::outputField(fieldHandle_t fieldHandle, float data){
-  std::cout << "outputField-float" << std::endl;
+  std::get<arrow::FloatBuilder>(builders[fieldHandle]).Append(data);
 }
 
 void StatisticOutputTest::outputField(fieldHandle_t fieldHandle, double data){
-  std::cout << "outputField-double" << std::endl;
+  std::get<arrow::DoubleBuilder>(builders[fieldHandle]).Append(data);
 }
 
 };  // end SST::Statistics
--- src/test/statOutputTest.h
+++ src/test/statOutputTest.h
@@ -2,8 +2,13 @@
 #ifndef STAT_OUTPUT_TEST_H_
 #define STAT_OUTPUT_TEST_H_
 
+#include <variant>
 #include <sst/core/sst_types.h>
 #include <sst/core/statapi/statoutput.h>
+
+#include <arrow/type.h>
+#include <arrow/api.h>
+
 
 namespace SST::Statistics{
 
@@ -46,6 +51,17 @@ public:
 
   StatisticOutputTest(Params& outputParameters);
 
+  using ArrowBuilders = std::variant<
+    std::monostate,
+    arrow::Int32Builder,
+    arrow::Int64Builder,
+    arrow::UInt32Builder,
+    arrow::UInt64Builder,
+    arrow::FloatBuilder,
+    arrow::DoubleBuilder,
+    arrow::NullBuilder
+  >;
+
 protected:
   bool checkOutputParameters() override;
 
@@ -71,6 +87,10 @@ protected:
   StatisticOutputTest()  : SSTDataBase() { ; }
 
 private:
+  std::vector<std::shared_ptr<arrow::RecordBatch>> batches;
+  std::vector<ArrowBuilders> builders;
+  std::vector<std::shared_ptr<arrow::Schema>> schemas;
+
 };  // end StatOutputTest
 };  // end SST::Statistics
 
