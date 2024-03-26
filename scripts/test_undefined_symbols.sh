16a60a1e9a26968e3d6b912f934fc068ad2ca381
jleidel <john.leidel@gmail.com>
2024-03-26 08:54:38 -0500

    updating copyrights

--- scripts/test_undefined_symbols.sh
+++ scripts/test_undefined_symbols.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+#
+# scrripts/test_undefined_symbols.sh
+#
+# Copyright (C) 2017-2024 Tactical Computing Laboratories, LLC
+# All Rights Reserved
+# contact@tactcomplabs.com
+# See LICENSE in the top level directory for licensing details
+#
 
 exec >&2
 
--- test/test1.py
+++ test/test1.py
@@ -1,3 +1,12 @@
+#
+# sst-data/test/test1.py
+#
+# Copyright (C) 2017-2024 Tactical Computing Laboratories, LLC
+# All Rights Reserved
+# contact@tactcomplabs.com
+# See LICENSE in the top level directory for licensing details
+#
+
 import sst
 
 # Define SST core options
